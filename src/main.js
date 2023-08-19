19a3d7fa20c5b7eec282b4bcfa3ae78920318a1d
Mário Gabriel <mgabrieldias10@gmail.com>
2023-08-18 21:22:14 -0300

    projeto finalizado

---
+++ src/main.js
@@ -0,0 +1,36 @@
+let weight = document.querySelector("#weight")
+let height = document.querySelector("#height")
+let button = document.querySelector(".calc-imc")
+let result = document.querySelector(".result")
+
+weight.focus()
+
+
+button.addEventListener("click", ()=> {
+
+    let imc = weight.value / (height.value * height.value)
+
+    if( imc < 18.5) {
+        result.innerHTML= `Seu imc é ${parseInt(imc)}, você está abaixo do peso!`
+    }
+
+
+    if (imc >=18.5 | imc < 24.9) {
+        result.innerHTML= `Seu imc é ${parseInt(imc)}, você está no peso ideal!`
+    }
+
+    if (imc >=25 | imc < 29.9) {
+        result.innerHTML= `Seu imc é ${parseInt(imc)}, você está com sobrepeso!`
+    }
+
+    if (imc >=30) {
+        result.innerHTML= `Seu imc é ${parseInt(imc)}, você está com obesidade!`
+    }
+
+    if (weight.value.length == "" | height.value.length == "") {
+        result.classList.add("error")
+        result.innerHTML= `Não foi possível calcular seu imc, você precisa preencher todos os campos necessários`
+        weight.focus()
+    }
+
+})
